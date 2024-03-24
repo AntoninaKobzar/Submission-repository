8474efb4d81b58115fa6f89a7f2c7842bd2f8cdd
Antonina Kobzar <antonina.kobzar@gmail.com>
2024-03-24 18:07:52 +0200

    2.6

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -6,20 +6,38 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+const handleNameChange=(e)=>{
+    setNewName(e.target.value)
+   
+  }
+
+const handleAddName=(e)=>{
+  e.preventDefault();
+ setPersons([...persons, { name: newName }]);
+ console.log(newName)
+ setNewName('');
+}
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleAddName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        <ul>
+        {persons.map((person)=>(
+        <li key={person.name}>{person.name}</li>
+      ))}
+      </ul>
+      </div>
     </div>
+
   )
 }
 
